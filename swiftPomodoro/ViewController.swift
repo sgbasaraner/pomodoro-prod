eb948edc823837c8871220fc89779bd631da612f
Sarp Basaraner <sarpbasaraner@gmail.com>
2017-11-24 14:23:36 +0300

    Initial Commit

---
+++ swiftPomodoro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftPomodoro
+//
+//  Created by Sarp Guney on 24.11.2017.
+//  Copyright © 2017 Sarp Guney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
