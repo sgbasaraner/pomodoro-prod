57c024ea9153ce1b6649b7e452fb01dbef9eaab6
Sarp Basaraner <sarpbasaraner@gmail.com>
2017-11-24 18:37:19 +0300

    add radiomodebutton class

---
+++ swiftPomodoro/TimerModeButton.swift
@@ -0,0 +1,13 @@
+//
+//  TimerModeButton.swift
+//  swiftPomodoro
+//
+//  Created by Sarp Guney on 24.11.2017.
+//  Copyright © 2017 Sarp Guney. All rights reserved.
+//
+
+import UIKit
+
+class TimerModeButton: UIButton {
+	
+}
