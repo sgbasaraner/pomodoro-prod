656375e83893d85567daac0977885cca800e2fc2
Sarp Basaraner <sarpbasaraner@gmail.com>
2017-11-24 15:48:40 +0300

    add primitive timer

--- swiftPomodoro/ViewController.swift
+++ swiftPomodoro/ViewController.swift
@@ -12,11 +12,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var timerLabel: UILabel!
     
+    var seconds = 0
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateTimer()
+    }
+    
+    func runTimer() {
+        timer = Timer.init(timeInterval: 1, target: self, selector: (#selector(updateTimer)), userInfo: nil, repeats: true)
+        
+        RunLoop.main.add(timer, forMode: RunLoopMode.commonModes)
+    }
+    
+    @objc func updateTimer() {
+        if seconds >= 1{
+            seconds -= 1
+        }
+        timerLabel.text = "\(seconds / 60):\(seconds % 60)"
     }
     
     @IBAction func pomodoroTouch(_ sender: UIButton) {
+        seconds = 25 * 60
+        runTimer()
     }
     
     @IBAction func shortBreakTouch(_ sender: UIButton) {
