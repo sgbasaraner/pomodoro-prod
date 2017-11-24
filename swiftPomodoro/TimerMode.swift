3acd6648a02c2cea0da3bda189a55b087bc3a757
Sarp Basaraner <sarpbasaraner@gmail.com>
2017-11-24 16:10:19 +0300

    create TimerMode struct

---
+++ swiftPomodoro/TimerMode.swift
@@ -0,0 +1,14 @@
+//
+//  TimerMode.swift
+//  swiftPomodoro
+//
+//  Created by Sarp Guney on 24.11.2017.
+//  Copyright © 2017 Sarp Guney. All rights reserved.
+//
+
+import Foundation
+
+struct TimerMode {
+    let name: String
+    let seconds: Int
+}
