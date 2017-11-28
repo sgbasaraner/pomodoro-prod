3b88990227d9d3b378b57bb07ef352b3d6d67d0e
Sarp Basaraner <sarpbasaraner@gmail.com>
2017-11-28 13:42:58 +0300

    add numberedcell class

---
+++ swiftPomodoro/NumberedCell.swift
@@ -0,0 +1,24 @@
+//
+//  NumberedCell.swift
+//  swiftPomodoro
+//
+//  Created by Sarp Guney on 28.11.2017.
+//  Copyright © 2017 Sarp Guney. All rights reserved.
+//
+
+import UIKit
+
+class NumberedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
