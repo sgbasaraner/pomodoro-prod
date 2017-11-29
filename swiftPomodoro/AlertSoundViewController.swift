4dbe784280e5d85732a88183662b32299eeb82b6
Sarp Basaraner <sarpbasaraner@gmail.com>
2017-11-29 10:48:22 +0300

    add alertsound table view controller

---
+++ swiftPomodoro/AlertSoundViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AlertSoundViewController.swift
+//  swiftPomodoro
+//
+//  Created by Sarp Guney on 29.11.2017.
+//  Copyright © 2017 Sarp Guney. All rights reserved.
+//
+
+import UIKit
+
+class AlertSoundViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+	}
+}
