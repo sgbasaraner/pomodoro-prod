d36021f0f48e184ad92ce486426ab7e740d9855c
Sarp Basaraner <sarpbasaraner@gmail.com>
2017-11-29 11:43:05 +0300

    populate table vc

--- swiftPomodoro/AlertSoundViewController.swift
+++ swiftPomodoro/AlertSoundViewController.swift
@@ -10,19 +10,26 @@ import UIKit
 
 class AlertSoundViewController: UITableViewController {
 
+	var soundsForDisplay = [String]()
+	
     override func viewDidLoad() {
         super.viewDidLoad()
-		prepareSoundsForDisplay(sounds: getSounds())
+		soundsForDisplay = prepareSoundsForDisplay(sounds: getSounds())
     }
 	
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return soundsForDisplay.count
+	}
+	
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+		cell.textLabel?.text = soundsForDisplay[indexPath.row]
+		
+		return cell
 	}
 	
 	func getSounds() -> [String] {
